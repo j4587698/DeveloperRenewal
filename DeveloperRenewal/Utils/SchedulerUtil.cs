c4167360c931f54f23d736fd9c5586102bc0fa23
j4587698 <jx@jvxiang.com>
2020-05-15 22:56:42 +0800

    添加定时任务

---
+++ DeveloperRenewal/Utils/SchedulerUtil.cs
@@ -0,0 +1,42 @@
+﻿using DeveloperRenewal.Entity;
+using FluentScheduler;
+using GraphLib;
+using GraphLib.Utils;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperRenewal.Utils
+{
+    public class SchedulerUtil
+    {
+        public static bool AddScheduler(int id)
+        {
+            var appliction = LiteDbHelper.Instance.GetDataById<ApplicationEntity>(nameof(ApplicationEntity), id);
+            if (appliction == null)
+            {
+                return false;
+            }
+            Random random = new Random();
+            var schedule = new Schedule(async () =>
+            {
+                Graph graph = new Graph(appliction.ClientId, appliction.ClientSecret, new PathString("/Home/Register"), Constants.Scopes);
+                var client = await graph.GetGraph();
+                var request = await client.Me.Messages.Request().Top(5).GetAsync();
+                
+            }, run => run.OnceIn(random.Next(appliction.MinExecInterval, appliction.MaxExecInterval)).Seconds());
+            schedule.JobEnded += (sender, e) =>
+            {
+                appliction = LiteDbHelper.Instance.GetDataById<ApplicationEntity>(nameof(ApplicationEntity), id);
+                if (appliction.AuthorizationStatus && appliction.IsEnable)
+                {
+                    schedule.SetScheduling(run => run.OnceIn(random.Next(appliction.MinExecInterval, appliction.MaxExecInterval)));
+                }
+                
+            };
+            return true;
+        }
+    }
+}
